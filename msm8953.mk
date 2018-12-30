330c481f31d3663a94911bb880fd11786ffcd4ec
Bruno Martins <bgcngm@gmail.com>
2018-12-30 19:58:56 +0000

    msm8953-common: Convert XiaomiDoze into XiaomiParts
     * This is the preparation for turning the package into
       an unified device-settings app.
    
    Change-Id: I425ad525edcc63c2dcccd1e2fb8459f442744970

--- msm8953.mk
+++ msm8953.mk
@@ -74,6 +74,10 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
+# Device-specific Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
@@ -145,10 +149,6 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     vendor.display.config@1.0 \
     vendor.display.config@1.0_vendor
-
-# Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
 
 # DRM
 PRODUCT_PACKAGES += \
