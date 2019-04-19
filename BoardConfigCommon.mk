4a6207fad193900975ff70b2347a91c3f8231f1c
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-04-19 16:42:58 -0300

    msm8953-common: Ignore neverallows

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -220,6 +220,7 @@ endif
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
