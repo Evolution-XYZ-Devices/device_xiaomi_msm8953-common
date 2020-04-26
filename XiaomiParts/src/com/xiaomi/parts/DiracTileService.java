7c9cf37e6c1ed823768e3000c334a8e8424a8b17
MASTERGUY <murtipipo@gmail.com>
2020-04-26 12:17:04 +0530

    msm8953-common: XiaomiParts Fixed Random Crahses because of Dirac Q/S Tile but processing might take time

--- XiaomiParts/src/com/xiaomi/parts/DiracTileService.java
+++ XiaomiParts/src/com/xiaomi/parts/DiracTileService.java
@@ -3,6 +3,8 @@ package com.xiaomi.parts;
 import android.app.ActivityManager;
 import android.content.Intent;
 import android.os.Handler;
+import android.content.Context;
+import android.os.Bundle;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 
@@ -13,7 +15,20 @@ public class DiracTileService extends TileService {
     @Override
     public void onStartListening() {
 
-        boolean enhancerEnabled = DiracService.sDiracUtils.isDiracEnabled();
+
+	boolean enhancerEnabled;
+        try {
+            enhancerEnabled = DiracService.sDiracUtils.isDiracEnabled();
+        } catch (java.lang.NullPointerException e) {
+            try {
+		getApplicationContext().startService(new Intent(getApplicationContext(), DiracService.class));
+                enhancerEnabled = DiracService.sDiracUtils.isDiracEnabled();
+            } catch (NullPointerException ne) {
+                // Avoid crash
+                ne.printStackTrace();
+                enhancerEnabled = false;
+            }
+        }
 
         Tile tile = getQsTile();
         if (enhancerEnabled) {
@@ -30,10 +45,12 @@ public class DiracTileService extends TileService {
     @Override
     public void onClick() {
         Tile tile = getQsTile();
-        if (DiracService.sDiracUtils.isDiracEnabled()) {
+	if (DiracService.sDiracUtils.isDiracEnabled()) {
+	    getApplicationContext().stopService(new Intent(getApplicationContext(), DiracService.class));
             DiracService.sDiracUtils.setEnabled(false);
             tile.setState(Tile.STATE_INACTIVE);
         } else {
+	    getApplicationContext().startService(new Intent(getApplicationContext(), DiracService.class));
             DiracService.sDiracUtils.setEnabled(true);
             tile.setState(Tile.STATE_ACTIVE);
         }
