731016d34059502e07cac190e0b7ca2d5c135efb
abhinavgupta371 <abhinavgupta371@gmail.com>
2020-04-30 17:58:28 +0000

    msm8953-common: fix some wiered date shit

--- prop.mk
+++ prop.mk
@@ -204,7 +204,8 @@ service.qti.ims.enabled=1
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.delta_time.enable=true \
-persist.timed.enable=true
+persist.timed.enable=true \
+persist.vendor.delta_time.enable=true
 
 # Tcp
 PRODUCT_PROPERTY_OVERRIDES += \
