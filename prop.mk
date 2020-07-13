d37555b359792bf79831fccd28eb4480261e4904
ProchyGaming <mcmovieslovakiasro@gmail.com>
2020-07-13 08:52:03 +0200

    msm8953-common: Fix ADB
    On enforcing builds it won't ask for authorization

--- prop.mk
+++ prop.mk
@@ -222,7 +222,7 @@ persist.vendor.usb.config.extra=none
 # USB debugging
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.sys.usb.config=mtp,adb \
-ro.adb.secure=0 \
+ro.adb.secure=1 \
 ro.secure=0 \
 ro.debuggable=1
 
