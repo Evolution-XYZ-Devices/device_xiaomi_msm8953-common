23349e1ef7bc2a783bb183be4d008e89e1758ad6
xyyx <xyyx@mail.ru>
2020-07-28 13:27:51 +0530

    msm8953-common: Add basic USB HAL that reports no status change
    Signed-off-by: ItsVixano <vixanonew@gmail.com>
    Change-Id: I46874b1d6a6491d07b7d5ae896a192312b4047cd

--- msm8953.mk
+++ msm8953.mk
@@ -345,7 +345,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.msm8953
 
 # VNDK
 PRODUCT_COPY_FILES += \
