861e1c76199655497f33c245a5a69ba13e0b57ad
Davide Garberi <dade.garberi@gmail.com>
2020-09-11 16:32:23 +0200

    msm8953-common: Add default dalvik heap values as fallback
    * In case libinit doesn't get loaded (for example on any pure AOSP GSI) we
      need to have fallback values to make the device usable
    
    Change-Id: I149be63a625d3b00188512b45fe0e556de6abaec

--- msm8953.mk
+++ msm8953.mk
@@ -7,6 +7,9 @@
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
