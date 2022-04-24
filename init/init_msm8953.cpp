7558a766dd280f2be7d7532b91b324eadf0ba709
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-04-24 18:15:51 +0900

    msm8953-common: Add init to handle dalvik props based on sysram
    * Tissot is a 4GB RAM device

--- init/init_msm8953.cpp
+++ init/init_msm8953.cpp
@@ -37,18 +37,10 @@
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
 
-#include "vendor_init.h"
 #include "property_service.h"
-
-using android::base::GetProperty;
-
-char const *heapstartsize;
-char const *heapgrowthlimit;
-char const *heapsize;
-char const *heapminfree;
-char const *heapmaxfree;
-char const *heaptargetutilization;
+#include "vendor_init.h"
 
 void property_override(char const prop[], char const value[], bool add = true)
 {
@@ -61,47 +53,23 @@ void property_override(char const prop[], char const value[], bool add = true)
     }
 }
 
-void check_device()
-{
+void load_dalvik_properties() {
     struct sysinfo sys;
 
     sysinfo(&sys);
-
-    if (sys.totalram > 5072ull * 1024 * 1024) {
-        // from - phone-xhdpi-6144-dalvik-heap.mk
-        heapstartsize = "16m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heaptargetutilization = "0.5";
-        heapminfree = "8m";
-        heapmaxfree = "32m";
-    } else if (sys.totalram > 3072ull * 1024 * 1024) {
+    if (sys.totalram > 3072ull * 1024 * 1024) {
         // from - phone-xxhdpi-4096-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heaptargetutilization = "0.6";
-        heapminfree = "8m";
-        heapmaxfree = "16m";
-    } else {
-        // from - phone-xhdpi-2048-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "192m";
-        heapsize = "512m";
-        heaptargetutilization = "0.75";
-        heapminfree = "512k";
-        heapmaxfree = "8m";
+        property_override("dalvik.vm.heapstartsize", "8m");
+        property_override("dalvik.vm.heapgrowthlimit", "192m");
+        property_override("dalvik.vm.heapsize", "512m");
+        property_override("dalvik.vm.heaptargetutilization", "0.6");
+        property_override("dalvik.vm.heapmaxfree", "16m");
+        property_override("dalvik.vm.heapminfree", "8m");
     }
 }
 
-void vendor_load_properties()
-{
-    check_device();
+void vendor_load_properties() {
+    property_override("ro.boot.verifiedbootstate", "green");
 
-    property_override("dalvik.vm.heapstartsize", heapstartsize);
-    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_override("dalvik.vm.heapsize", heapsize);
-    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
-    property_override("dalvik.vm.heapminfree", heapminfree);
-    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+    load_dalvik_properties();
 }
