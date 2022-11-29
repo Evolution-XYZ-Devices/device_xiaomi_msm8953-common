e9dfd70662c762b3e9e4647d417c1645c7410d06
Lafactorial <hknhzl@gmail.com>
2022-11-29 16:04:19 +0300

    Revert "msm8953-common: switch to crDroid doze package"
    This reverts commit 4c1fb4ecc2ea3abd0389fc23b97d9a2d26f93639.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
+import org.lineageos.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -31,6 +32,10 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        if (DozeUtils.isDozeEnabled(context) && DozeUtils.sensorsEnabled(context)) {
+            if (DEBUG) Log.d(TAG, "Starting Doze service");
+            DozeUtils.startService(context);
+        }
         new DiracUtils(context).onBootCompleted();
     }
 }
