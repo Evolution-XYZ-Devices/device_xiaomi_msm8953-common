37d30e239b727bc77721d767df8d26c257c87256
Giovanni Ricca <giovanniricca@proton.me>
2023-09-05 21:20:33 +0200

    msm8953-common: Switch to fragmented defconfigs
    Change-Id: I5c3a7c41a9764221a2872830abf078ecc2cdf192

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,6 +34,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
 TARGET_KERNEL_LLVM_BINUTILS := true
 BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true
+TARGET_KERNEL_CONFIG := msm8953-perf_defconfig xiaomi/xiaomi.config
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
