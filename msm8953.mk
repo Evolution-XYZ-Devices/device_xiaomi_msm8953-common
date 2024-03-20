6b735261543ca9cdc6395c1eb853d89f087daff3
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    msm8953-common: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- msm8953.mk
+++ msm8953.mk
@@ -8,7 +8,6 @@
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # APEX
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 
 # Overlays
