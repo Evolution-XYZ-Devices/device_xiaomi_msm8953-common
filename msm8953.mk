4bf3958b7db929b1288921a2cc0aca4b6c6e3cdc
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:56:09 +0300

    msm8953-common: Switch to QTI vibrator HAL
    Change-Id: I911001c14167f7dfceb609090f7b90117310da07

--- msm8953.mk
+++ msm8953.mk
@@ -394,8 +394,10 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Whitelisted app
 PRODUCT_COPY_FILES += \
