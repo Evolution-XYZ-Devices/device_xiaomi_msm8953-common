8a13c5d0c5245cebe968ee57b34c31813290dcc9
Giovanni Ricca <giovanniricca@proton.me>
2024-06-20 01:18:02 +0200

    msm8953-common: Add `atcmdfwd` on framework compatibility matrix
    Change-Id: I9943e6510a014523c3e13e2ff3599c5a1bd6135f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -91,6 +91,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/lineage/config/device_framework_matrix.xml
