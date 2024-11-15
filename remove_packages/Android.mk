ec98dc3eafe12c3531e2c673d608214bf15f60a0
Joey Huab <joey@evolution-x.org>
2024-11-15 19:57:15 +0000

    msm8953-common: Update remove_packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AudioFX GoogleTTS Photos PixelWallpapers2021 talkback SafetyHubPrebuilt WellbeingPrebuilt
+LOCAL_OVERRIDES_PACKAGES := Aperture AudioFX Backgrounds Calendar2 Calendar Etar PicoTts
+LOCAL_OVERRIDES_PACKAGES += DeviceAsWebcam Gallery2 Glimpse PhotoTable Recorder
+LOCAL_OVERRIDES_PACKAGES += GoogleTTS SafetyHubPrebuilt WellbeingPrebuilt
+LOCAL_OVERRIDES_PACKAGES += Photos TagGoogle talkback OdadPrebuilt
+LOCAL_OVERRIDES_PACKAGES += PrebuiltBugle bcr CreativeAssistant
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
