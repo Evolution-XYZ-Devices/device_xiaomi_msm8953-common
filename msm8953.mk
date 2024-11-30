39805f8772f485b35412bbe634877e69fedc6aff
Giovanni Ricca <giovanniricca@proton.me>
2024-11-30 18:01:50 +0100

    msm8953-common: Build `wcnss_service` from source (again)
    Change-Id: Iac6e5c5b9d42509a95b4fc1fb3450f4fb7ff0d14

--- msm8953.mk
+++ msm8953.mk
@@ -152,8 +152,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0-service-qti \
     libbatching \
     libgeofencing \
-    libgnss \
-    libwifi-hal-ctrl
+    libgnss
 
 PRODUCT_PACKAGES += \
     flp.conf \
@@ -342,6 +341,7 @@ PRODUCT_PACKAGES += \
     libwifi-hal-ctrl \
     libwpa_client \
     hostapd \
+    wcnss_service \
     wificond \
     WifiOverlay \
     wpa_supplicant \
